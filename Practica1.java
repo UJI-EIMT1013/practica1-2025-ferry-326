7d95aa8af15e125c5ee8b53a00fb125fba126969
ferran <al448047@uji.es>
2025-10-08 10:57:26 +0200

    ejercicio1

--- Practica1.java
+++ Practica1.java
@@ -2,18 +2,33 @@ package practica1;
 
 import java.util.*;
 
+
 public class Practica1 {
 
     //EJERCICIO 1
     public static Set<Integer> multiplos (Iterator<Integer> it) {
-        //TODO
-        return null;
+        Set<Integer> elementos= new HashSet<Integer>();
+        Set<Integer> multiplos= new HashSet<Integer>();
+        while(it.hasNext()){
+            int elem= it.next();
+            if(elementos.contains(elem) && elem!=0) multiplos.add(elem);
+            if(elem!=0) elementos.add(elem);
+        }
+        for(int a:elementos){
+            for(int b:elementos){
+                if(a%b==0 && a!=b) multiplos.add(a);
+            }
+        }
+        return multiplos;
     }
 
     //EJERCICIO2
     public static void separate (Set<Integer> cuadrados, Set<Integer> noCuadrados)  {
-        //TODO
-
+        Iterator<Integer> it=cuadrados.iterator();
+        while(it.hasNext()){
+            int elem=it.next();
+            if(Math.sqrt(elem))
+        }
     }
 
     //EJERCICIO 3
